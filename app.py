f5cfb0c49136c516f2b93989ab07d0f10aa759ed
Sadik Korkmaz <123670994+KingSadik1@users.noreply.github.com>
2023-05-16 06:38:48 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ import plotly_express as px
 
 df = pd.read_csv('vehicles_us.csv')
 #read the data
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 df['is_4wd'] = df['is_4wd'].where(df['is_4wd'] != 1.0 , 'yes')
 df['is_4wd'] = df['is_4wd'].fillna('no')
+df['model_year'] = df['model_year'].fillna(df['model_year'].mean()).astype('int')
+df['cylinders'] = df['cylinders'].fillna(df['cylinders'].mean()).astype('int')
+df['odometer'] = df['odometer'].fillna(df['odometer'].mean()).astype('int')
+df['paint_color'] = df['paint_color'].fillna('No info')
 #replace four wheel drive values with yes and no
 st.title('Figures on car sales')
 
@@ -19,8 +24,17 @@ st.dataframe(df)
 #i will now create a data set with all missing values in the odometer collumn removed
 
 df_model_year = df.dropna(subset = ['odometer'])
-st.header('Scatter Plot: click on scatter points to see car model and other details')
-fig = px.scatter(df , x = 'odometer', 
+st.header('Scatter Plot: Price VS Odometer (make a selection to see cars by condition)')
+cond_list = sorted(df['condition'].unique())
+condition = st.selectbox(
+    label= 'select condition',
+    options= cond_list,
+    index= cond_list.index('new')
+)
+df_conditon = df[df['condition'] == condition]
+
+fig = px.scatter(df_conditon , 
+                 x = 'odometer', 
                  y = 'price', 
                  color = 'condition',
                  title = 'Odometer vs Price (Make selection to see different car condition selections)',
@@ -29,8 +43,25 @@ fig = px.scatter(df , x = 'odometer',
 #also contains name of car
 st.write(fig)
 
-st.header('Histogram: Manfacturer and fuel type (make a fuel type selection)')
+st.header('Histogram: Manfacturer and odometer readings')
 
-df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+manufac_list = sorted(df['manufacturer'].unique())
+manufacturer_1 = st.selectbox('Select manufacturer 1',
+                              manufac_list, index=manufac_list.index('chevrolet'))
 
-st.write(px.histogram(df, x='manufacturer', color='fuel',opacity = .7))
+manufacturer_2 = st.selectbox('Select manufacturer 2',
+                              manufac_list, index=manufac_list.index('hyundai'))
+mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
+df_filtered = df[mask_filter]
+normalize = st.checkbox('Normalize histogram', value=True)
+if normalize:
+    histnorm = 'percent'
+else:
+    histnorm = None
+st.write(px.histogram(df_filtered,
+                      x='odometer',
+                      nbins=30,
+                      color='manufacturer',
+                      histnorm=histnorm,
+                      barmode='overlay'))
+
